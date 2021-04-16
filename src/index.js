e59c2ac0bc0447bca0ab2a8fb8a7cb02dc657b06
Nazmul <nazmulhasanfci@gmail.com>
2021-04-17 00:26:50 +0600

    add content

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-ReactDOM.render(<h1>hello</h1>,
-  document.getElementById('root')
+ReactDOM.render(
+  <>
+  <h1>Top Five NetFlix Movies</h1>
+  <p>list of 5 best movies</p>
+  <ol>
+    <li>Waterworld.</li>
+    <li>Accepted.</li>
+    <li>Friends with Benefits.</li>
+    <li>The Little Rascals.</li>
+    <li>Legally Blonde.</li>
+  </ol>
+  </>
+,document.getElementById('root')
 );
 
